5a1d99a9c58064a2cc282457c7a8f8b1ed53065c
Le Hoang Vu <elhoangvu@gmail.com>
2018-04-21 23:05:30 +0700

    add node and config files

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+var fs = require('fs');
+
+//var Cart = require('../models/cart'); <- model cart
+var products = [] //Fetch data at here
+
+router.get('/', function (req, res, next) {
+  res.render('index', 
+  { 
+    title: 'Trilpe Shop',
+    products: products
+  }
+  );
+});
+
+module.exports = router;
