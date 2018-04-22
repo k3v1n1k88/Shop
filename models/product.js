c5f4d8317ed5db856ac3e0b16d757dd243f4618a
Le Hoang Vu <elhoangvu@gmail.com>
2018-04-23 01:02:59 +0700

    update index, views,... change image path

---
+++ models/product.js
@@ -0,0 +1,13 @@
+module.exports = function Product(prod) {
+	this.name = prod.name;
+	this.id = prod.id;
+	this.image = prod.image;
+	this.price = prod.price;
+	this.view = prod.view;
+	this.buy = prod.buy;
+	this.description = prod.description;
+	this.origin = prod.origin;
+	this.type = prod.type;
+	this.manufacturer = prod.manufacturer;
+	this.date = prod.date;
+}
